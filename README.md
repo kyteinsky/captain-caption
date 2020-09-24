ee197e1f5b57070fbd28ab38e9c1b78c30528b57
kyte <kyteinsky@gmail.com>
2020-09-24 14:55:24 +0000

    model trained!

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # captain-caption
+
+-- (photo illustration goes here) --
+---
+
+## Initial steps (NECESSARY STEPS)
+
+- install miniconda (or anaconda)
+- then git clone this repo
+- now you might want to run this code sniplet `conda env create -f environment.yml` (if your cudatoolkit version doesn't match your Nvidia driver version, uninstall and reinstall it)
+- just get into the env created `conda activate captain-caption`
+
+
+## Understanding the config.py file
+
+It's just a file to tune hyperparams and some other things--
+- checkout the file first
+- `cpt` is to activate (or deactivate) storing of checkpoints for the model
+- `wb` is for turning on wandb logging and tracking
+
+*for training only*
+- `npy` is activate for first time to store all feature vectors (it'll take 41G for the whole dataset, so have enough disk space available before proceeding to train)
+One can change the sample_size in `config.py` to around 30000 for test training, *you-know-what-I-mean!*
+
+
+## If you are interested in training from ground up, follow these steps:
+- here you need to download the train data (COCO dataset 2014), use [this link](http://images.cocodataset.org/zips/train2014.zip)
+- ITS ~13G file
+- extract it into the repo's root dir
+- run `python train.py` with `npy = True` for generation of feature vectors
+- after that set `npy = False`
+- finally you need to run `python train.py` and see the ***magic happen*** . . .
+
+### In case you need visuals for your model, signup at wandb.com
+Create an experiment there with name `captioning` or your own custom name, in that case you need to change `line no. 61` of `train.py` file
+then you need to run this command
+`wandb login <YOUR API KEY from the website>`
+-- OR -- simply `wandb login`
+that's it!
+
+
+
+## In the case you are looking to just check out the performace of the model -- not done yet
+- put images to caption in `test_images` folder
+- run `python inference.py` with `npy = False` in `config.py`
+
+`exit()`
---
+++ helper.py
@@ -0,0 +1,139 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+import tensorflow as tf
+import numpy as np
+from config import hyperparams
+
+npy_dir, _, _, BATCH_SIZE, BUFFER_SIZE, embedding_dim, units, _, _, _, _, _, _ = hyperparams()
+
+def load_image(image_path):
+    img = tf.io.read_file(image_path)
+    img = tf.image.decode_jpeg(img, channels=3)
+    img = tf.image.resize(img, (299, 299))
+    img = tf.keras.applications.inception_v3.preprocess_input(img)
+    return img, image_path
+
+def calc_max_length(tensor):
+    return max(len(t) for t in tensor)
+
+# Load the numpy files
+def map_func(img_name, cap):
+    img_tensor = np.load(os.path.join(npy_dir, os.path.basename(img_name).decode('utf-8'))+'.npy')
+    return img_tensor, cap
+
+
+def tfdataset(img_name_train, cap_train):
+    dataset = tf.data.Dataset.from_tensor_slices((img_name_train, cap_train))
+
+    # Use map to load the numpy files in parallel
+    dataset = dataset.map(lambda item1, item2: tf.numpy_function(
+            map_func, [item1, item2], [tf.float32, tf.int32]),
+            num_parallel_calls=tf.data.experimental.AUTOTUNE)
+
+    # Shuffle and batch
+    dataset = dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
+    dataset = dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+    return dataset
+
+
+
+#############################################################-----------MODEL----------------###########        START
+
+class BahdanauAttention(tf.keras.Model):
+  def __init__(self, units):
+    super(BahdanauAttention, self).__init__()
+    self.W1 = tf.keras.layers.Dense(units)
+    self.W2 = tf.keras.layers.Dense(units)
+    self.V = tf.keras.layers.Dense(1)
+
+  def call(self, features, hidden):
+    # features(CNN_encoder output) shape == (batch_size, 64, embedding_dim)
+
+    # hidden shape == (batch_size, hidden_size)
+    # hidden_with_time_axis shape == (batch_size, 1, hidden_size)
+    hidden_with_time_axis = tf.expand_dims(hidden, 1)
+
+    # attention_hidden_layer shape == (batch_size, 64, units)
+    attention_hidden_layer = (tf.nn.tanh(self.W1(features) +
+                                         self.W2(hidden_with_time_axis)))
+
+    # score shape == (batch_size, 64, 1)
+    # This gives you an unnormalized score for each image feature.
+    score = self.V(attention_hidden_layer)
+
+    # attention_weights shape == (batch_size, 64, 1)
+    attention_weights = tf.nn.softmax(score, axis=1)
+
+    # context_vector shape after sum == (batch_size, hidden_size)
+    context_vector = attention_weights * features
+    context_vector = tf.reduce_sum(context_vector, axis=1)
+
+    return context_vector, attention_weights
+
+
+
+class CNN_Encoder(tf.keras.Model):
+    # Since you have already extracted the features and dumped it using pickle
+    # This encoder passes those features through a Fully connected layer
+    def __init__(self, embedding_dim):
+        super(CNN_Encoder, self).__init__()
+        # shape after fc == (batch_size, 64, embedding_dim)
+        self.fc = tf.keras.layers.Dense(embedding_dim)
+
+    def call(self, x):
+        x = self.fc(x)
+        x = tf.nn.relu(x)
+        return x
+
+
+class RNN_Decoder(tf.keras.Model):
+    def __init__(self, embedding_dim, units, vocab_size):
+        super(RNN_Decoder, self).__init__()
+        self.units = units
+
+        self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
+        self.gru = tf.keras.layers.GRU(self.units,
+                                        return_sequences=True,
+                                        return_state=True,
+                                        recurrent_initializer='glorot_uniform')
+        self.fc1 = tf.keras.layers.Dense(self.units)
+        self.fc2 = tf.keras.layers.Dense(vocab_size)
+
+        self.attention = BahdanauAttention(self.units)
+
+    def call(self, x, features, hidden):
+        # defining attention as a separate model
+        context_vector, attention_weights = self.attention(features, hidden)
+
+        # x shape after passing through embedding == (batch_size, 1, embedding_dim)
+        x = self.embedding(x)
+
+        # x shape after concatenation == (batch_size, 1, embedding_dim + hidden_size)
+        x = tf.concat([tf.expand_dims(context_vector, 1), x], axis=-1)
+
+        # passing the concatenated vector to the GRU
+        output, state = self.gru(x)
+
+        # shape == (batch_size, max_length, hidden_size)
+        x = self.fc1(output)
+
+        # x shape == (batch_size * max_length, hidden_size)
+        x = tf.reshape(x, (-1, x.shape[2]))
+
+        # output shape == (batch_size * max_length, vocab)
+        x = self.fc2(x)
+
+        return x, state, attention_weights
+
+    def reset_state(self, batch_size):
+        return tf.zeros((batch_size, self.units))
+
+#############################################################-----------MODEL----------------###########        END
+
+
+
+
+
+
+
+
